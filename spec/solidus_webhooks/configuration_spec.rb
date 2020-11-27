1a524e5182a638a0462e42ce8c1f808104a6a126
Elia Schito <elia@schito.me>
2020-11-27 18:46:31 +0100

    Fix Rubocop errors and refine the rules

--- spec/solidus_webhooks/configuration_spec.rb
+++ spec/solidus_webhooks/configuration_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe SolidusWebhooks::Configuration do
   describe '#register_webhook_handler' do
     it 'only accepts callable' do
       expect{ subject.register_webhook_handler(:foo, Object.new) }.to raise_error(SolidusWebhooks::InvalidHandler)
-      expect{ subject.register_webhook_handler(:bar, Proc.new {}) }.not_to raise_error
+      expect{ subject.register_webhook_handler(:bar, proc {}) }.not_to raise_error
     end
   end
 end
